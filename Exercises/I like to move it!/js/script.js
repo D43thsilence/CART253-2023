b2fe5e2f7faa9c88d279161f931916a6664f38d2
D43thsilence <malcolm.tadonki@gmail.com>
2023-09-17 15:02:38 -0400

    Major overhaul on I like to move it!

--- Exercises/I like to move it!/js/script.js
+++ Exercises/I like to move it!/js/script.js
@@ -16,27 +16,30 @@ function preload() {
 }
 
 // Sets up each variable
-let ellipseXPosition = 100;
-let ellipseYPosition = 150;
-let ellipseSize = 50;
-let altEllipseSize = 25;
-let ellipseXMove = 2;
+let ellipseXPosition = 450;
+let ellipseYPosition = 450;
+let ellipseSize = 200;
+let altEllipseSize = 100;
+let ellipseFill = 0;
+let mouthMouvement = 30;
 
-let rectXPosition = 100;
-let rectYPosition = 300;
+let rectXPosition = 450;
+let rectYPosition = 450;
+let rectXMove = 0
 
 let hexXPosition;
 let hexYPosition;
 
 let circleSize = 50;
+let circleFill = 0
 
 // Sets up the javascript object
 
     // let ellipseData = {
-    x: 100
-    y: 150
-    size: 50
-    speed: 2
+    // x: 100,
+    // y: 150,
+    // size: 50,
+    // speed: 2
     // };
 
 
@@ -46,31 +49,59 @@ let circleSize = 50;
 */
 function setup() {
 createCanvas(900,900);
+noStroke();
+
 
 }
 
 
 /**
  * Description of draw()
- * Draws, fills and animates the shapes
+ * Draws, fills, sets random numbers and animates the shapes
 */
 function draw() {
-    background(255,205,100)
-    fill(100, 204, 0)
-    // ellipseSize += 1
-    // ellipseXPosition += ellipseXMove
+    background(255,205,100);
+
+    // Calculates the randomNumber value
+    let randomNumber = random (-5,5);
+
+    // Draws the rectangle in the background
+    fill(175, 100, 0);
+    rectMode(CENTER);
+    // rectXPosition = rectXPosition + 2;
+    rectXPosition = constrain(rectXPosition,0,850);
+    rect(rectXPosition,rectYPosition,height,width);
+
+    fill(255,205,100);
+    rect(rectXPosition,rectYPosition,800,600);
+    
+    // Draws the character
+    fill(80, 100, 0);
+    ellipse(450, 900 ,800,500);
+    ellipse(450, 450 ,500,500);
+    mouthMouvement = mouthMouvement + randomNumber
+    mouthMouvement = constrain (mouthMouvement,10,50)
+    fill(0, 0, 0);
+    ellipse(450, 620 ,100, mouthMouvement);
+
+    // Draws the moving Eye
+    ellipseMode(CENTER)
+    ellipseFill = random (0,255);
+    fill(ellipseFill);
     ellipse(ellipseXPosition, ellipseYPosition ,ellipseSize);
 
-    fill(255,205,100)
-    // altEllipseSize = altEllipseSize + 1.40
-    ellipse(ellipseXPosition, ellipseYPosition ,altEllipseSize);
+    
+    fill(255,205,100);
+    altEllipseSize = altEllipseSize + randomNumber;
+    altEllipseSize = constrain(altEllipseSize,80,120);
+    ellipseXPosition = ellipseXPosition + randomNumber;
+    ellipseXPosition = constrain (ellipseXPosition, 380,510)
+    ellipse(ellipseXPosition, ellipseYPosition ,altEllipseSize,200);
 
-    fill(255, 204, 0)   
-    rect(rectXPosition,rectYPosition,150,150);
-
+    // Draws the circle that follows your mouse
     fill(255, 100, 0)
-    // circleSize = circleSize + 3
-    circle (mouseX, mouseY, circleSize)
+    circleSize = map(mouseX,width,0,100,300);
+    circle (mouseX, mouseY, circleSize);
    
 
 
