de34a8766716eef7e157d848eceb51a6e34480cf
D43thsilence <malcolm.tadonki@gmail.com>
2023-09-17 09:04:32 -0400

    Updated I like to move it!

--- Exercises/I like to move it!/js/script.js
+++ Exercises/I like to move it!/js/script.js
@@ -2,29 +2,47 @@
  * I like to move it!
  * Malcolm Siné Tadonki
  * 
- * This is my project for the I like to move it! exercise
+ * This is my project for the I like to move it! exercise where I make shapes and animations move
  */
 
 "use strict";
 
 /**
  * Description of preload
+ * does nothing
 */
 function preload() {
 
 }
 
-let elipseXPosition;
-let elipseYPosition;
+// Sets up each variable
+let ellipseXPosition = 100;
+let ellipseYPosition = 150;
+let ellipseSize = 50;
+let altEllipseSize = 25;
+let ellipseXMove = 2;
 
-let rectXPosition;
-let rectYPosition;
+let rectXPosition = 100;
+let rectYPosition = 300;
 
 let hexXPosition;
 let hexYPosition;
 
+let circleSize = 50;
+
+// Sets up the javascript object
+
+    // let ellipseData = {
+    x: 100
+    y: 150
+    size: 50
+    speed: 2
+    // };
+
+
 /**
  * Description of setup
+ * Creates the canvas
 */
 function setup() {
 createCanvas(900,900);
@@ -34,9 +52,26 @@ createCanvas(900,900);
 
 /**
  * Description of draw()
+ * Draws, fills and animates the shapes
 */
 function draw() {
-    background(255,0,0);
-    ellipse(250,250,circleSize);    
+    background(255,205,100)
+    fill(100, 204, 0)
+    // ellipseSize += 1
+    // ellipseXPosition += ellipseXMove
+    ellipse(ellipseXPosition, ellipseYPosition ,ellipseSize);
+
+    fill(255,205,100)
+    // altEllipseSize = altEllipseSize + 1.40
+    ellipse(ellipseXPosition, ellipseYPosition ,altEllipseSize);
+
+    fill(255, 204, 0)   
+    rect(rectXPosition,rectYPosition,150,150);
+
+    fill(255, 100, 0)
+    // circleSize = circleSize + 3
+    circle (mouseX, mouseY, circleSize)
+   
+
 
 }
