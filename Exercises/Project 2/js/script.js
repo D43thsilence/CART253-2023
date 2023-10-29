88216393a2e92a6d7477a4aa9a9c3763f255f708
D43thsilence <malcolm.tadonki@gmail.com>
2023-10-29 14:53:26 -0400

    Began working on Project 2

---
+++ Exercises/Project 2/js/script.js
@@ -0,0 +1,31 @@
+/**
+ * Project 2
+ * Malcolm Siné Tadonki
+ * 
+ * This project is a battle simulator, use your skills appropriately in order to defeat a powerful foe!
+ */
+
+"use strict";
+
+/**
+ * Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+ * Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+ * Description of draw()
+*/
+function draw() {
+
+}
